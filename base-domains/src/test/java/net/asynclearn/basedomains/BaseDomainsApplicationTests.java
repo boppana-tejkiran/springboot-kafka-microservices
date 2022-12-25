ebb9e961d2682c912b0a5b07fc2f1cd9969d2e84
tejkiran <kirantejboppana@gmail.com>
2022-12-25 11:06:53 +0530

    Create Micro Services

---
+++ base-domains/src/test/java/net/asynclearn/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.asynclearn.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ email-service/src/test/java/net/asynclearn/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.asynclearn.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
